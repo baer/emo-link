860d4c84a5d319674c9e7d3c2549a140930ce5c5
Eric Baer <me@ericbaer.com>
2023-03-31 11:43:35 -0700

    Add redirects + KV access

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
 import { encodeUUIDAsEmoji, generateURISafeEmojiUUID } from "./emoji-id";
 
 export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-  //
-  // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-  // MY_SERVICE: Fetcher;
+  EMO_LINK: KVNamespace;
 }
 
 export default {
@@ -20,23 +10,37 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    const uuid = crypto.randomUUID();
-    const uuidAsEmoji = encodeUUIDAsEmoji(uuid);
-    const uriEncodedEmojiUUID = encodeURIComponent(uuidAsEmoji);
+    const url = new URL(request.url);
+    const { pathname } = url;
 
-    const html = `<!DOCTYPE html>
-      <body>
-        <h1>EmoShort</h1>
-        <p>UUID: ${uuid}</p>
-        <p>Emoji: ${uuidAsEmoji}</p>
-        <p>URI Encoded: ${uriEncodedEmojiUUID}</p>
-      </body>
-    `;
+    if (pathname === "/") {
+      const uuid = crypto.randomUUID();
+      const uuidAsEmoji = encodeUUIDAsEmoji(uuid);
+      const uriEncodedEmojiUUID = encodeURIComponent(uuidAsEmoji);
 
-    return new Response(html, {
-      headers: {
-        "content-type": "text/html;charset=UTF-8",
-      },
-    });
+      const html = `<!DOCTYPE html>
+        <body>
+          <h1>EmoShort</h1>
+          <p>UUID: ${uuid}</p>
+          <p>Emoji: ${uuidAsEmoji}</p>
+          <p>URI Encoded: ${uriEncodedEmojiUUID}</p>
+        </body>
+      `;
+
+      return new Response(html, {
+        headers: {
+          "content-type": "text/html;charset=UTF-8",
+        },
+      });
+    }
+
+    const redirectURL = await env.EMO_LINK.get(pathname);
+    if (!redirectURL) {
+      return new Response(`📯 😢 Womp womp: '${pathname}' was not found.`, {
+        status: 404,
+      });
+    }
+
+    return Response.redirect(redirectURL, 301);
   },
 };
