1b5bbcbb5d7d342a53ffe176684e5ebcb7ec996b
Eric Baer <me@ericbaer.com>
2023-04-04 17:48:54 -0700

    Improve documentation for UUID generation

--- src/server/emoji-id.ts
+++ src/server/emoji-id.ts
@@ -1,16 +1,5 @@
 import emojiAlphabet from "./emoji-alphabet.json";
 
-// I have 4764 emoji in my alphabet require 11 bits to encode a truncated set
-// (4096) or 13 bits for the complete set (8192). I've chosen to use 12 since
-// padding several thousand chars wouldconsiderably complicate the problem for
-// no real benefit
-//
-// Using 12 bits per char, you will need 11 chars to encode a UUID.
-// UUID Combinations = 2^122 (122 random bits + 4 to specify version/variant)
-// 2^122 - 4096^10 ~=  3.99×10^36
-// 2^122 - 4096^11 ~= -2.24 x 10^19
-const BITS_PER_CHARACTER = 12;
-
 /**
  * Converts a hexadecimal number to an 8-bit binary string.
  * @param hexNum - A hexadecimal number as a string.
@@ -31,21 +20,39 @@ function chunkString(str: string, length: number): string[] {
 }
 
 /**
- * Encodes a UUID as a string of Emoji from the Unicode 15 set.
+ * Encodes a UUID as a string of 11 Emoji from the Unicode 15 set.
+ *
+ * Explaination:
+ *   UUID Combinations = 2^122 (122 random bits + 4 to specify version/variant)
+ *   2^122 - 4096^10 ~=  3.99×10^36
+ *   2^122 - 4096^11 ~= -2.24 x 10^19
+ *
  * @param uuid - A UUID string.
  * @returns A string of emojis representing the input UUID.
  */
 export function encodeUUIDAsEmoji(uuid: string): string {
+  // Convert the UUID to a binary string
   const uuidAsBinaryString = chunkString(uuid.replaceAll(`-`, ``), 2)
     .map(hexToBinary)
     .join(``);
 
-  return chunkString(uuidAsBinaryString, BITS_PER_CHARACTER)
+  // The alphabet size determines the number of bits required to encode the
+  // complete set. The Unicode 15 Emoji set includes 4764 emojis. This
+  // requires either 12 bits to encode a slightly truncated set (2^12 = 4096), of
+  // 13 bits for the complete set. I've chosen to use 12 since padding several
+  // thousand chars would considerably complicate the problem for no real benefit
+  const bitsPerEmoji = 12;
+
+  // Convert the binary string to an emoji string using the emoji alphabet
+  return chunkString(uuidAsBinaryString, bitsPerEmoji)
     .map((binaryNumber: string) => parseInt(binaryNumber, 2))
     .map((emojiIndex: number) => emojiAlphabet[emojiIndex])
     .join("");
 }
 
+/**
+ * @returns A URL-safe emoji-encoded UUID string.
+ */
 export function generateURISafeEmojiUUID(): string {
   const uuid = crypto.randomUUID();
   const uuidAsEmoji = encodeUUIDAsEmoji(uuid);
